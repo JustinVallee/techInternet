759d1f9909d0873d1ab8e96e89fcd0c64f6e7b7c
Justin Vallée <62106699+JustinVallee@users.noreply.github.com>
2022-03-04 02:09:12 -0500

    Co-authored-by: yAmineee <yAmineee@users.noreply.github.com>

---
+++ carnet.js
@@ -0,0 +1,45 @@
+/** Stockage = localStorage;
+//Test pour le local storage
+localStorage.setItem('Justin', 'Exemple de keys');
+
+localStorage.setItem('Yamine', 'Exemple de keys');
+localStorage.setItem('Vallée', 'Exemple de keys');
+localStorage.setItem('Ibrahima', 'Exemple de keys');
+
+
+const crypto = require('crypto') // appel de la bibliothèque crypto intégré à nodejs qui permet de générer des Clefs
+
+**/
+
+//Création de clefs
+import { isCryptoKey } from 'util/types';
+
+const crypto = require('crypto')
+const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: { type: 'spki', format: 'pem' },
+  privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+})
+
+console.log('Private key : ' + privateKey);
+console.log('\nPublic key : ' + publicKey);
+
+const fs = require('fs');
+fs.writeFile('privateKey.pem', privateKey, function(err) {
+  if (err) throw err
+})
+
+fs.writeFile('publicKey.pem', publicKey, function(err) {
+  if (err) throw err
+})
+
+/** Utilisation du Js pour modifier la page
+ * 
+ * let para = document.querySelector('p'); // Sélectionner un élément du document
+
+  para.addEventListener('click', updateName); // Création d'un event listener (avec appel de la fonction update name )
+
+  function updateName() {
+  let name = prompt('Enter a new name');
+  para.textContent = 'Player 1: ' + name;
+} **/
